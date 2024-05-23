cbf15259eb91dcf73d3e68f4b5995c54ff8f0ea2
Sumit Dhakal <sumit@Sumits-MacBook-Pro.local>
2024-05-23 22:49:47 +0545

    added error handeling

---
+++ server/utils/error/AppError.ts
@@ -0,0 +1,18 @@
+class AppError extends Error{
+    statusCode:number
+    status:string
+    isOperational:any
+    path?: string; // Make path optional with ?
+    value?: any;
+    constructor(message:any, statusCode:any){
+        super(message)
+        this.statusCode = statusCode
+        this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+        this.isOperational = true
+        Error.captureStackTrace(this, this.constructor);
+
+
+    }
+}
+
+export default AppError
