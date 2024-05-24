3e7ca236229b97cfa2b2d172c2a840aea253f87b
Sumit Dhakal <sumit@Sumits-MacBook-Pro.local>
2024-05-24 13:17:10 +0545

    feat:added update controller

--- server/controller/userController.ts
+++ server/controller/userController.ts
@@ -1,4 +1,30 @@
 import User from '../model/userModel'
+import { Request,Response,NextFunction } from 'express'
 import {getAll,createOne} from './factoryHandler'
+import AppError from '../utils/error/AppError'
+import { catchAsync } from '../utils/error/catchAsync'
+
 export const getOneUser = getAll(User)
 export const createOneUser = createOne(User)
+
+
+export const updatedMe =catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    if (req.body.password) {
+        return next(
+            new AppError('This route is not for password updates. Please use /updateMyPassword.', 400)
+        )
+    }
+
+    const object = req.body
+    
+
+    const updateUser = await User.findByIdAndUpdate(req.params.id, object, {
+        new: true,
+        runValidation: true
+    });
+    res.status(200).json({
+        data: {
+            user: updateUser
+        }
+    })
+})
