4d13ee654a5eabb907dcab0b454a8824fcba7c5b
Sumit Dhakal <sumit@Sumits-MacBook-Pro.local>
2024-05-24 13:18:46 +0545

    feat:added login and signup route

--- server/routes/userRoute.ts
+++ server/routes/userRoute.ts
@@ -1,8 +1,10 @@
 import {getOneUser,createOneUser} from '../controller/userController'
 import express from 'express'
-
+import {signup,login} from '../controller/authController'
 const router = express.Router()
 
+router.post('/user/signup',signup)
+router.post('/user/login',login)
 router.route('/user').get(getOneUser).post(createOneUser)
 
 export default router
