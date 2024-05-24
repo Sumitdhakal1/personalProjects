541a5420d0df8f835ee61fc9caefd998fe4949eb
Sumit Dhakal <sumit@Sumits-MacBook-Pro.local>
2024-05-24 11:59:34 +0545

    added interface

--- server/model/userModel.ts
+++ server/model/userModel.ts
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose'
 import { CallbackWithoutResultAndOptionalError } from "mongoose";
 import bcrypt from 'bcrypt'
-const userSchema = new mongoose.Schema({
+import {IUser} from '../utils/types/interface'
+const userSchema = new mongoose.Schema<IUser>({
     userName:{
        type:String,
        unique:true
@@ -28,7 +29,7 @@ userSchema.pre('save',async function(next:CallbackWithoutResultAndOptionalError)
     const user = this
     if(!user.isModified('password')) return(next);
     try{
-        const hashedPassword = await bcrypt.has(user.password,12);
+        const hashedPassword = await bcrypt.hash(user.password,12);
         user.password = hashedPassword
         next()
     }catch(error){
