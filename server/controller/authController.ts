6a61a5919ef219494b3ec80f0dc6710346b92479
Sumit Dhakal <sumit@Sumits-MacBook-Pro.local>
2024-05-24 11:58:01 +0545

    feat:added login logout and protect controller

--- server/controller/authController.ts
+++ server/controller/authController.ts
@@ -3,6 +3,11 @@ import AppError from "../utils/error/AppError";
 import {isEmailValid, passwordValidator} from '../utils/validator'
 import User from "../model/userModel";
 import { catchAsync } from "../utils/error/catchAsync";
+import {decodeToken,createSendToken} from '../utils/jwtToken'
+import bcrypt from 'bcrypt'
+import cookie from 'cookie'
+
+
 export const signup =catchAsync(async (req: Request, res: Response, next:NextFunction) => {
 
       const { email, password} = req.body
@@ -32,3 +37,78 @@ export const signup =catchAsync(async (req: Request, res: Response, next:NextFun
         }
       }) 
   })
+
+  export const login =catchAsync(async (req: Request, res: Response, next:NextFunction) => {
+
+      const { email, password } = req.body
+      if (!email || !password) {
+        return next(new AppError('please provide your email or password', 401))
+  
+      }
+      const user = await User.findOne({ email }).select('+password')
+      if(!user || !(await bcrypt.compare(password, user.password)) ){
+        return next(new AppError('incorrect email or password', 401))
+    }
+       const token = await createSendToken(user._id);
+       const id = user._id
+       res.setHeader('set-Cookie',cookie.serialize('name', token as string, {
+        httpOnly:true,
+        maxAge: 1*1000*60,
+        secure:true
+       }))
+     res.status(200).json({
+        status:'success',
+        data:{
+            id,
+            token
+        }
+     })
+
+  
+  })
+  
+  
+  export const protect = async (req: Request, res: Response, next: NextFunction) => {
+    let token;
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith('Bearer ')
+    ) {
+      token = req.headers.authorization.split(' ')[1];
+    }
+    
+  
+    if (!token) {
+     return next(new AppError('You are not logged in! Please log in to get access.',404))
+    }
+  
+   const data = process.env.JWT_SECRET as string
+    decodeToken(token,data,next)
+  }
+  
+  
+   
+  export const logout = async(req:Request, res:Response, next:NextFunction)=>{
+  
+   res.setHeader('set-Cookie',cookie.serialize('name', 'loggedOut', {
+       httpOnly:true,
+       maxAge: 0,
+       secure:true
+      }))
+      res.status(200).json({
+       message:'success'
+      })
+  
+   }
+
+   export const removeCookie = async(req:Request, res:Response, next:NextFunction)=>{
+    const cookieName =res.clearCookie('name')
+    
+    if(!cookieName){
+    return next(new AppError('You are not logged in!.',404))
+    }else{
+      next()
+    }
+    
+    }
+      
