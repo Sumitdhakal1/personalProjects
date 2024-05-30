714b58164955e846997007ac8a317272a81c3c52
Sumit Dhakal <sumit@Sumits-MacBook-Pro.local>
2024-05-30 22:13:36 +0545

    feat:search query using pokemon name

--- server/controller/pokeApiController.ts
+++ server/controller/pokeApiController.ts
@@ -4,10 +4,25 @@ import pokemonDatabase from '../database/apiCall'
 import ApiFeatures from '../utils/apiFeatures'
 
 export const getAll = catchAsync(async(req:Request,res:Response)=>{
-  const data = await pokemonDatabase()
+  const features =  new ApiFeatures( pokemonDatabase(req), req.query)
+  const data = await features.query
   res.status(200).json({
     data
   })
+})
+
+export const getOne = catchAsync(async(req:Request, res:Response)=>{
+  const query =(req.query.q as string).toLowerCase()
+  console.log(`Search Query: ${query}`)
+  const result = await pokemonDatabase(req)
+  console.log(result)
+  const data =  result.filter((item:any)=>{ 
+     const itemName = item.name.toLowerCase()
+     const includeQuery = itemName.includes(query)
+     return includeQuery
+  })
+  console.log(`Filtered Data: ${JSON.stringify(data)}`);
+  res.send(data)
 })
 
 
